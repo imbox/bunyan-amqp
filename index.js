43289b2a888ce903d1c7c8afa513987d851340d6
Mathias Lundell <lundell.mathias@gmail.com>
2016-10-17 16:10:04 +0200

    Replace package amqp with rabbot

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict'
-let amqp = require('amqp')
+let rabbot = require('rabbot')
+
+let noop = function () {}
 
 class BunyanTransport {
   constructor (options) {
@@ -12,46 +14,42 @@ class BunyanTransport {
       60: 'fatal'
     }
 
-    let context = this
-    this.exchangePromise = new Promise(function (resolve, reject) {
-      context.connection = amqp.createConnection({
-        heartbeat: 5,
-        heartbeatForceReconnect: true,
-        host: options.host || '127.0.0.1',
-        port: options.port || 5672,
-        login: options.user,
-        password: options.password
-      })
-
-      context.connection.once('ready', function () {
-        let opts = options.exchange.options || {}
-        let name = options.exchange.name
-        context.connection.exchange(name, opts, function (ex) {
-          resolve(ex)
-        })
-      })
-
-      context.connection.on('error', function (err) {
-        throw err
-      })
+    this.connectionName = 'mq-log-connection'
+    this.connection = rabbot.addConnection({
+      name: this.connectionName,
+      user: options.user,
+      pass: options.password,
+      host: options.host || '127.0.0.1',
+      port: options.port || 5672,
+      timeout: 2000,
+      heartbeat: 5
     })
+
+    this.exchangeName = options.exchange.name
+    let exchangeOptions = options.exchange.options || {}
+    this.exchange = rabbot.addExchange(
+      this.exchangeName, 'topic', exchangeOptions, this.connectionName
+    )
   }
 
   write (message) {
-    let context = this
-    this.exchangePromise.then(function (exchange) {
-      let routingKey = [
-        message.hostname.replace('.', ':'),
-        message.name.replace('.', ':'),
-        context.levels[message.level]
-      ].join('.')
-      let headers = {contentType: 'application/json', timestamp: new Date()}
-      exchange.publish(routingKey, JSON.stringify(message), headers)
-    })
-  }
+    let connectionName = this.connectionName
+    let connection = this.connection
+    let exchange = this.exchange
+    let exchangeName = this.exchangeName
+    let routingKey = [
+      message.hostname.replace('.', ':'),
+      message.name.replace('.', ':'),
+      this.levels[message.level]
+    ].join('.')
 
-  close () {
-    this.connection.disconnect()
+    connection.then(exchange).then(function () {
+      rabbot.publish(exchangeName, {
+        routingKey: routingKey,
+        body: message
+      }, connectionName)
+    })
+    .catch(noop)
   }
 }
 
